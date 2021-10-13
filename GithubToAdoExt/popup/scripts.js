ab6f5508526db32cbe32042c2ba99c3c0d9e90b1
Kristen Schau <krschau@users.noreply.github.com>
2021-10-13 10:03:12 -0700

    Don't make user click button, just show link immediately

--- GithubToAdoExt/popup/scripts.js
+++ GithubToAdoExt/popup/scripts.js
@@ -1,10 +1,11 @@
-function getTabs()
-{
-    chrome.tabs.query({currentWindow: true, active: true}, doThingWithTabs);
-}
+window.onload = (event) => {
+    // Gets the current tabs
+    chrome.tabs.query({currentWindow: true, active: true}, getAdoLink);
+};
 
-function doThingWithTabs(tabs)
+function getAdoLink(tabs)
 {
+    // Check the URL of the current tab to see if it's a MUX GitHub URL
     let fullUrl = tabs[0].url;
     if (isValidUrl(fullUrl))
     {
@@ -14,40 +15,35 @@ function doThingWithTabs(tabs)
         // Create link with search URL
         document.getElementById("content").innerHTML = 
             "<a href='" + searchUrl + "' "
-            + "target='_blank' rel='noopener' " // Opens link in new tab
-            + "title='Open ADO link in new tab'>Link</a>";
+            + "target='_blank' rel='noopener' "   // Opens link in new tab
+            + "title='Open ADO link in new tab' " // Hover text
+            + ">ADO Link</a>";                    // Display text
     }
     else
     {
-        //document.write("Not a WinUI GitHub page");
         document.getElementById("content").innerHTML = "Not a WinUI GitHub page";
     }
 }
 
 function isValidUrl(fullUrl)
 {
-    let firstPart = "https://github.com/microsoft/microsoft-ui-xaml/issues/";
-    let isValid = (fullUrl.startsWith(firstPart)) ? true : false;
-    return isValid;
+    // Leave out ending slash so the full list of issues (no number) is considered valid
+    let firstPart = "https://github.com/microsoft/microsoft-ui-xaml/issues";
+    return fullUrl.startsWith(firstPart);
 }
 
 function getGithubNumberFromUrl(fullUrl)
 {
-    let number;
     let firstPart = "https://github.com/microsoft/microsoft-ui-xaml/issues/";
-    let len = firstPart.length;
-    number = fullUrl.substring(len);
-    return number;
+    return fullUrl.substring(firstPart.length);
 }
 
 function createAdoSearchUrl(githubNumber)
 {
     let urlStart = "https://microsoft.visualstudio.com/OS/_search?";
-    let searchTest = "text=https%3A//github.com/microsoft/microsoft-ui-xaml/issues/";
-    // github item number goes here
+    let searchText = "text=https%3A//github.com/microsoft/microsoft-ui-xaml/issues/";
+    // github item number goes here (if there is one, otherwise we return a query for all the MUX GitHub issues)
     let itemType = "&type=workitem&lp=workitems-Project";
-    let searchUrl = urlStart + searchTest + githubNumber + itemType;
+    let searchUrl = urlStart + searchText + githubNumber + itemType;
     return searchUrl;
 }
-
-document.getElementById("linkButton").addEventListener("click", getTabs);
--- README.md
+++ README.md
@@ -3,17 +3,19 @@ A Chromium extension for linking to MSFT internal ADO items from WinUI 3 GitHub 
 
 ## How install
 Currently, the only way to install this extention into Edge is to sideload it as an unpackaged extension.
-1. Download the repo, or just the GithubToAdoExt directory.
-2. Open the Extensions pane in Edge (this can be found under the ... menu in the upper right)
-3. Ensure that "Developer mode" is turned on (bottom left corner. If it's not there, hit the hamburger menu in the upper left.)
+1. Download the repo, or just the GithubToAdoExt directory
+2. Open the Extensions pane in Edge (this can be found under the "..." menu in the upper right)
+3. Ensure that "Developer mode" is turned on. (There is a toggle in the bottom left corner. 
+   If it's not there, hit the hamburger menu in the upper left.)
 4. Click the "Load unpackaged" icon
 5. Select the GithubToAdoExt folder
 
 The extension should now be installed, and you should see the WinUI logo in your Edge toolbar.
 
 ## How to use
-- Go to the WinUI GitHub page you want to find the ADO item for.
+- Go to the WinUI GitHub page you want to find the ADO item for
 - Click the extension's icon in the toolbar
-- Click GetLink
-- Click the link
-If you are not on a WinUI GitHub page, you will see an error stating such.
+- Clicking the link provided will open the ADO item in a new tab
+
+If you are not on a WinUI GitHub page, you will see an error stating such. Using the extension from 
+the main GitHub issues page will bring you to a query for all ADO issues mirrored from the MUX GitHub.
